47831f35df9da0eec2c279bf34588b6dd4967c44
ysy <1156546473@qq.com>
2016-08-16 17:47:12 +0800

    clean code

--- app/component/If.js
+++ app/component/If.js
@@ -3,7 +3,6 @@ import {View} from 'react-native'
 
 export default class If extends Component {
     render() {
-        console.log(this.props.v)
         return this.props.v ? this.props.children : null;
     }
 }
