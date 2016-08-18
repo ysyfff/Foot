5fbe25930e7fde903557c2c6b3da124af7cfeb90
ysy <1156546473@qq.com>
2016-08-18 18:12:03 +0800

    clean code

--- app/src/foot/Foot.js
+++ app/src/foot/Foot.js
@@ -1,35 +1,31 @@
 import React, {Component} from 'react'
 import {Text, View, MapView, AsyncStorage} from 'react-native'
 import ViewContainer from '../common/ViewContainer'
-import Store from '../common/Store'
 // import MapView from 'react-native-maps'
 
 import {MapViewExample, LocationFollow, Callout, Overlay} from '../example/test/testMapView'
+import _ from 'lodash'
 
-console.log(Store, 'll')
+//Test MapView Example
+// export default class Foot extends Component {
+//     render() {
+//         return (
+//             <ViewContainer style={{backgroundColor: 'green'}} noNav={true}>
+//                 <LocationFollow />
+//                 <Overlay />
+//                 <Callout nav={this.props.nav} />
+//                 <MapViewExample />
+//             </ViewContainer>
+//         )
+//     }
+// }
+
 export default class Foot extends Component {
-    // render() {
-    //     return (
-    //         <ViewContainer style={{backgroundColor: 'green'}} noNav={true}>
-    //             <LocationFollow />
-    //             <Overlay />
-    //             <Callout nav={this.props.nav} />
-    //             <MapViewExample />
-    //         </ViewContainer>
-    //     )
-    // }
     constructor(props) {
         super(props)
 
     }
     render() {
-        var key = 'point334';
-        Store.setPoint(key, [{a: 1, b: 2}], (err, val) => {
-            AsyncStorage.getItem(key, (err, val) => {
-                console.log(val)
-            })
-        });
-
         //MapView不能套在ViewContainer中，否则导致MapView的flex：1时，高度无法伸展(应该是scroll属性导致)
         return (
             <View style={{flex: 1}}>
@@ -41,5 +37,13 @@ export default class Foot extends Component {
             </View>
         )
     }
-
+    _getPoint(key=new Date(): String, callback=()=>{}: Function) {
+        AsyncStorage.getItem(key, callback);
+    }
+    _setPoint(key=new Date(): String, data: [{a: 1,b:2}], callback = ()=>{}: Function) {
+        AsyncStorage.getItem(key, (err, val) => {
+            const point = val == null ? data : _.concat(JSON.parse(val), data);
+            AsyncStorage.setItem(key, JSON.stringify( point ), callback);
+        });
+    }
 }
