a0cf47ec48a9839fa6fdf22b99e1685b30cb7790
ysy <1156546473@qq.com>
2016-08-18 16:49:28 +0800

    add AsyncStorage

--- app/src/foot/Foot.js
+++ app/src/foot/Foot.js
@@ -1,9 +1,10 @@
 import React, {Component} from 'react'
-import {Text, View, MapView} from 'react-native'
+import {Text, View, MapView, AsyncStorage} from 'react-native'
 import ViewContainer from '../common/ViewContainer'
 // import MapView from 'react-native-maps'
 
 import {MapViewExample, LocationFollow, Callout, Overlay} from '../example/test/testMapView'
+import _ from 'lodash'
 
 export default class Foot extends Component {
     // render() {
@@ -16,7 +17,18 @@ export default class Foot extends Component {
     //         </ViewContainer>
     //     )
     // }
+    constructor(props) {
+        super(props)
+
+    }
     render() {
+        var key = 'point333';
+        this._setPoint(key, [{a: 1, b: 2}], (err, val) => {
+            AsyncStorage.getItem(key, (err, val) => {
+                console.log(val)
+            })
+        });
+
         //MapView不能套在ViewContainer中，否则导致MapView的flex：1时，高度无法伸展(应该是scroll属性导致)
         return (
             <View style={{flex: 1}}>
@@ -27,5 +39,17 @@ export default class Foot extends Component {
                   />
             </View>
         )
+    }
+    _getPoint(key: String, callback = ()=>{}: Function) {
+        AsyncStorage.get(key, callback);
+    }
+    _setPoint(key: String, data: [{a: 1,b:2}], callback = ()=>{}: Function) {
+        AsyncStorage.getItem(key, (err, val) => {
+            const point = val == null ? data : _.concat(JSON.parse(val), data);
+
+            AsyncStorage.removeItem(key, (err) => {
+                AsyncStorage.setItem(key, JSON.stringify( point ), callback);
+            });
+        });
     }
 }
