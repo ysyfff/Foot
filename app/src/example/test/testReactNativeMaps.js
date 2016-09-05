f27332bd876668b5dc40e182b0180c79c3e0d170
ysy <1156546473@qq.com>
2016-09-05 17:37:24 +0800

    add level_degree

--- app/src/example/test/testReactNativeMaps.js
+++ app/src/example/test/testReactNativeMaps.js
@@ -6,16 +6,40 @@ import _ from 'lodash'
 import Btn from '../../../component/Btn'
 import Geo from './testGeolocation'
 var cacheRegion = {};
+
+const LEVEL_DEGREE = {
+    1: 180, //whole world
+    2: 90,
+    3: 45,
+    4: 22.5,
+    5: 11.25,
+    6: 5.625,
+    7: 2.831,
+    8: 1.406,
+    9: 0.703, //wide area
+    10: 0.352,
+    11: 0.176, //area
+    12: 0.088,
+    13: 0.044, //village or town
+    14: 0.022,
+    15: 0.011,
+    16: 0.005, // samll road
+    17: 0.003,
+    18: 0.001,
+    19: 0.0005,
+};
+const BASE_M_PIXEL = 0.298;
 export default class RNMapViews extends Component {
     constructor(props) {
         super(props)
         this.state = {
             region: {
-                latitude: 39.06,
-                longitude: -95.22,
-                latitudeDelta: 0.0922,
-                longitudeDelta: 0.0922,
-            }
+                latitude: 39.97647153065555,
+                longitude: 116.29967273616768,
+                latitudeDelta: 0.005,
+                longitudeDelta: 0.005,
+            },
+            level: 16
         }
     }
     _onRegionChangeComplete(region) {
